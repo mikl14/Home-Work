c5f7e47fc01f9038436f3575049a5edbed4b97d6
mikl14 <mikl.eli2002@gmail.com>
2023-12-19 19:33:53 +0300

    Метод getRandomAnimal() перенесен из Интерфейса в CreateAnimalServiceLmpl Вместе с методом генерации случайного имени, GetRandomAnimal() ,больше не принимает параметры.

--- src/main/java/ru/mts/animalsCreators/CreateAnimalService.java
+++ src/main/java/ru/mts/animalsCreators/CreateAnimalService.java
@@ -8,62 +8,6 @@ import java.util.Random;
 public interface CreateAnimalService
 {
     /**
-     *  Метод <b>getRandomAnimal</b>
-     * @return случайное животное, генерирует 4 вида животных с уникальными параметрами
-     *  */
-default AbstractAnimal getRandomAnimal() {
-        var random = new Random();
-        var name = generateRandomName(random);
-        return generateRandomAnimal(random, name);
-    }
-
-    private String generateRandomName(Random random) {
-        var nameBuilder = new StringBuilder();
-        for (int j = 0; j < random.nextInt(10 - 4) + 4; j++) {
-            nameBuilder.append((char) (random.nextInt(10) + 'A'));
-        }
-        return nameBuilder.toString();
-    }
-
-    private AbstractAnimal generateRandomAnimal(Random randomise, String name) {
-
-
-        switch (randomise.nextInt(4)) {
-            case 0:
-                return new Cat("Порода №" + (randomise.nextInt(1000)),
-                        name,
-                        "Кошачий",
-                        BigDecimal.valueOf(randomise.nextDouble() * 1000),
-                        "Кошачий корм",
-                        randomise.nextInt(12 - 9) + 9);
-            case 1:
-                return new Fish("Порода №" + (randomise.nextInt(1000)),
-                        name,
-                        "Молчунья",
-                        BigDecimal.valueOf(randomise.nextDouble() * 1000),
-                        "Рыбий корм",
-                        randomise.nextInt(10 - 5) + 10);
-            case 2:
-                return new Wolf("Порода №" + (randomise.nextInt(1000)),
-                        name,
-                        "Серый",
-                        BigDecimal.valueOf(randomise.nextDouble() * 1000),
-                        "Лес",
-                        randomise.nextInt(150 - 80) + 80);
-            case 3:
-                return new Bear("Порода №" + (randomise.nextInt(1000)),
-                        name,
-                        "Горящий",
-                        BigDecimal.valueOf(randomise.nextDouble() * 1000),
-                        "Тайга",
-                        randomise.nextInt(80 - 40) + 40);
-            default:
-                return null;
-        }
-
-    }
-
-    /**
      * Метод <b>getAnimals</b>
      * Был Перегружен в <b>CreateAnimalServiceLmpl<b> по т.з
      * @see CreateAnimalServiceLmpl#getAnimals()
@@ -71,11 +15,12 @@ default AbstractAnimal getRandomAnimal() {
      */
     default AbstractAnimal[] getAnimals()
     {
+        CreateAnimalServiceLmpl createAnimalServiceLmpl = new CreateAnimalServiceLmpl();
         int i = 0;
         AbstractAnimal[] animalArray = new AbstractAnimal[10];
         while(i < 10)
         {
-            animalArray[i] = getRandomAnimal();   // получаем случайного зверя через getRandomAnimal()
+            animalArray[i] = createAnimalServiceLmpl.getRandomAnimal();   // получаем случайного зверя через getRandomAnimal()
             i++;
         }
         return animalArray;
--- src/main/java/ru/mts/animalsCreators/CreateAnimalServiceLmpl.java
+++ src/main/java/ru/mts/animalsCreators/CreateAnimalServiceLmpl.java
@@ -1,16 +1,74 @@
 package ru.mts.animalsCreators;
 
-import ru.mts.Animals.AbstractAnimal;
+import ru.mts.Animals.*;
+
+import java.math.BigDecimal;
+import java.util.Random;
 
 public class CreateAnimalServiceLmpl implements CreateAnimalService
 {
+    Random random = new Random();
+
+    /**
+     *  Метод <b>generateRandomName</b>
+     * @return строку случайных символов со случайной длинной в диапазоне от 4 до 10
+     *  */
+    private String generateRandomName()
+    {
+        var nameBuilder = new StringBuilder();
+        for (int j = 0; j < random.nextInt(10 - 4) + 4; j++)
+        {
+            nameBuilder.append((char) (random.nextInt(10) + 'A'));
+        }
+        return nameBuilder.toString();
+    }
+    /**
+     *  Метод <b>getRandomAnimal</b>
+     * @return случайное животное, генерирует 4 вида животных с уникальными параметрами
+     *  */
+    public AbstractAnimal getRandomAnimal() {
+
+        String name = generateRandomName();
+
+        switch (random.nextInt(4)) {
+            case 0:
+                return new Cat("Порода №" + (random.nextInt(1000)),
+                        name,
+                        "Кошачий",
+                        BigDecimal.valueOf(random.nextDouble() * 1000),
+                        "Кошачий корм",
+                        random.nextInt(12 - 9) + 9);
+            case 1:
+                return new Fish("Порода №" + (random.nextInt(1000)),
+                        name,
+                        "Молчунья",
+                        BigDecimal.valueOf(random.nextDouble() * 1000),
+                        "Рыбий корм",
+                        random.nextInt(10 - 5) + 10);
+            case 2:
+                return new Wolf("Порода №" + (random.nextInt(1000)),
+                        name,
+                        "Серый",
+                        BigDecimal.valueOf(random.nextDouble() * 1000),
+                        "Лес",
+                        random.nextInt(150 - 80) + 80);
+            case 3:
+                return new Bear("Порода №" + (random.nextInt(1000)),
+                        name,
+                        "Горящий",
+                        BigDecimal.valueOf(random.nextDouble() * 1000),
+                        "Тайга",
+                        random.nextInt(80 - 40) + 40);
+            default:
+                return null;
+        }
+
+    }
+
     /**
      *
      * @param numberAnimals количество уникальных животных которых необходимо создать
      * @return Массив животных длинной numberAnimals
-     *
-     * использует дефолтный метод <b>getRandomAnimal()</b> из интерфейса <b>CreateAnimalService</b>
-     * @see CreateAnimalService#getRandomAnimal()
      */
 
     public AbstractAnimal[] getAnimals(int numberAnimals) {
