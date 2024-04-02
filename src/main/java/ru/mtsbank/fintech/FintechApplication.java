12bfd59f69f94873d6dd43945b14a75f6306fb4d
mikl14 <mikl.eli2002@gmail.com>
2024-04-03 00:04:30 +0300

    Удалены лишние imports

--- src/main/java/ru/mtsbank/fintech/FintechApplication.java
+++ src/main/java/ru/mtsbank/fintech/FintechApplication.java
@@ -1,19 +1,8 @@
 package ru.mtsbank.fintech;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import ru.mts.animals.AbstractAnimal;
-import ru.mts.animals.Bear;
-import ru.mts.animals.Cat;
-
-import java.io.File;
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.List;
 
 @SpringBootApplication
 @EnableScheduling
