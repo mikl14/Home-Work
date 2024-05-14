c1378fab630da21b73fc147689282c3122d2c2d9
mikl14 <107858531+mikl14@users.noreply.github.com>
2024-05-14 15:24:01 +0300

    Удалены ненужные import изменен readMe на актуальный

--- src/main/java/ru/mtsbank/fintech/controller/UIAnimalController.java
+++ src/main/java/ru/mtsbank/fintech/controller/UIAnimalController.java
@@ -1,6 +1,5 @@
 package ru.mtsbank.fintech.controller;
 
-import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +7,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.mtsbank.fintech.entity.Animal;
 import ru.mtsbank.fintech.service.AnimalService;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
 
 @Controller
 public class UIAnimalController {
