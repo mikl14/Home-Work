1804bddb42544df932c343991b0b16fbfa1758f1
mikl14 <mikl.eli2002@gmail.com>
2024-05-29 20:51:34 +0300

    Устранено редактирование файлов миграции

--- src/main/resources/db/migration/V1_3_0__animalsFill.sql
+++ src/main/resources/db/migration/V1_3_0__animalsFill.sql
@@ -45,20 +45,3 @@ VALUES
 (3,3),
 (4,4);
 
-INSERT INTO animals.role
-(role_name)
-VALUES
-('USER'),
-('ADMIN');
-
-INSERT INTO animals.animal_user
-(name,password)
-VALUES
-('mikl','schef2002'),
-('test','pass');;
-
-INSERT INTO animals.user_roles
-(user_id,role_id)
-VALUES
-(1,2),
-(2,1);
